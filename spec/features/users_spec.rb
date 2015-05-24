35e108202bd514706f24995b5b016aabedafbc1a
holrock <ispeporez@gmail.com>
2015-05-24 21:57:03 +0900

    add login example

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 feature do
-  scenario "ユーザー新規作成" do
+  scenario "register" do
     visit root_path
     expect {
       click_link "Register"
@@ -10,7 +10,18 @@ feature do
       fill_in "user_password", with: "pass"
       fill_in "user_password_confirmation", with: "pass"
       click_button "Create User"
-    }.to change(User, :count).by(1)n
+    }.to change(User, :count).by(1)
     expect(current_path).to eq login_path
   end
+
+  scenario "login" do
+    skip "not implement yet"
+    visit root_path
+    click_link "Login"
+    fill_in "email", with: "hoge@example.com"
+    fill_in "password", with: "pass"
+    click_button "Login"
+    expect(current_path).to eq users_path
+  end
+
 end
