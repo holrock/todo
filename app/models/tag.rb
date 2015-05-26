3a71b5dbc80528adbc3eead49add8c20822360c2
holrock <ispeporez@gmail.com>
2015-05-27 00:37:52 +0900

    add duplicate tag example

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,5 @@ class Tag < ActiveRecord::Base
   has_many :todo_items, through: :todo_tags
 
   validates :text, presence: true
+  validates :text, uniqueness: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Tag, type: :model do
       expect(Fg.build(:tag, text:nil)).not_to be_valid
     end
   end
+
+  context "duplicate tag" do
+    it "invalid" do
+      Fg.create(:tag, text:"tag")
+      tag = Fg.build(:tag, text:"tag")
+      tag.valid?
+      expect(tag.errors[:text]).to include("has already been taken")
+    end
+  end
+
 end
