ef40b17b699ab9c67745e9375c4c12a7c09a4f36
holrock <ispeporez@gmail.com>
2015-05-26 21:23:10 +0900

    update db schama

--- db/migrate/20150526044437_create_todo_lists.rb
+++ db/migrate/20150526044437_create_todo_lists.rb
@@ -1,16 +1,9 @@
 class CreateTodoLists < ActiveRecord::Migration
   def change
     create_table :todo_items do |t|
-      t.belongs_to :todo_list
+      t.belongs_to :user
       t.string :text
       t.boolean :finished
-      t.timestamps null: false
-    end
-
-    create_table :todo_lists do |t|
-      t.belongs_to :user
-      t.string :title
-      t.integer :list_order
       t.timestamps null: false
     end
   end
