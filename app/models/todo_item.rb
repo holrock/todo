153cc7cfe1bfdaefb2bb4a881c8aaca1db866239
holrock <ispeporez@gmail.com>
2015-05-26 19:08:46 +0900

    add scope

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,5 +2,6 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todo_list, dependent: :destroy
 
   validates :text, presence: true
-  validates :todo_list_id, presence: true
+
+  scope :with_list, lambda {|list| where(todo_list_id: list.id)}
 end
