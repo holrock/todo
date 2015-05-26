d91b48425bb63a4dde4674e7c3849c5715cc7318
holrock <ispeporez@gmail.com>
2015-05-26 13:52:25 +0900

    rewrite spce description

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,52 +6,52 @@ Fg ||= FactoryGirl
 RSpec.describe User, type: :model do
 
   context "パスワードが３文字以上のときは" do
-    it "有効であるべき" do
+    it "有効" do
       user = Fg.build(:user, password: "123", password_confirmation: "123")
       expect(user).to be_valid
     end
   end
 
   context "パスワードが３文字未満のときは" do
-    it "無効であるべき" do
+    it "無効" do
       user = Fg.build(:user, password: "12", password_confirmation: "12")
       expect(user).not_to be_valid
     end
   end
 
   context "パスワードと確認パスワードが違うときは" do
-    it "無効であるべき" do
+    it "無効" do
       user = Fg.build(:user, password: "123", password_confirmation: "124")
       expect(user).not_to be_valid
     end
   end
 
   context "パラメーターが有効なときは" do
-    it "有効であるべき" do
+    it "有効" do
       user = Fg.build(:user)
       expect(user).to be_valid
     end
   end
 
   context "パラメーターが無効なとき" do
-    it "emailが無効なら無効であるべき" do
+    it "emailが無効なら無効" do
       user = Fg.build(:user, email: nil)
       expect(user).not_to be_valid
     end
 
-    it "passswordが無効なら無効であるべき" do
+    it "passswordが無効なら無効" do
       user = Fg.build(:user, password: nil)
       expect(user).not_to be_valid
     end
 
-    it "passsword_confirmationが無効なら無効であるべき" do
+    it "passsword_confirmationが無効なら無効" do
       user = Fg.build(:user, password_confirmation: nil)
       expect(user).not_to be_valid
     end
   end
 
   context "重複したemail addressがあるときは" do
-    it "弾くべき" do
+    it "弾く" do
       Fg.create(:user, email: "hoge@example.com")
       user = Fg.build(:user, email: "hoge@example.com")
       user.valid?
