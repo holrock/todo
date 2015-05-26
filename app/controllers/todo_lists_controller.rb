cbdc4bb1a8c292e2aa98fb924f56bf9bd9a70e53
holrock <ispeporez@gmail.com>
2015-05-27 01:07:22 +0900

    add tag list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,9 +1,11 @@
 class TodoListsController < ApplicationController
-  #before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
 
   def list
-    @todo_items = current_user.todo_items.order("updated_at DESC")
+    @todo_items = current_user.todo_items.order(updated_at: :desc)
     @new_todo = TodoItem.new
+    @tags = Tag.joins(:todo_items)
+      .where(todo_items:{user_id: current_user.id})
+      .order(updated_at: :desc)
   end
 
 end
