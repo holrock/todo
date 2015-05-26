d61a1a15f5bea8c5793d8057e7c43d4165851417
holrock <ispeporez@gmail.com>
2015-05-26 16:27:07 +0900

    add with_user scope

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,7 +1,8 @@
 class TodoList < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, inverse_of: :todo_lists
   has_many :todo_items
 
-  validates :user_id, presence: true
   validates :title, presence: true
+
+  scope :with_user, lambda {|user| where(user_id: user.id)}
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe TodoList, type: :model do
       expect(list).not_to be_valid
     end
   end
+
+  describe "with_user検索時" do
+    it "同一ユーザーだけ返す" do
+      f1 = Fg.create(:todo_list)
+      f2 = Fg.create(:todo_list, user:f1.user)
+      Fg.create(:todo_list)
+
+      lists = TodoList.with_user(f1.user)
+      expect(lists).to match_array([f1, f2])
+    end
+  end
 end
