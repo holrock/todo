4ac674c1017dcbcdb6131930d37a5c1bd7b3da77
holrock <ispeporez@gmail.com>
2015-05-26 15:35:18 +0900

    add user_id validation

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -2,5 +2,6 @@ class TodoList < ActiveRecord::Base
   belongs_to :user
   has_many :todo_items
 
+  validates :user_id, presence: true
   validates :title, presence: true
 end
--- spec/factories/todo_lists.rb
+++ spec/factories/todo_lists.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :todo_list do
     title "List1"
     list_order 1
+    association :user, factory: :user
   end
 end
