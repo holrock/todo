f6b1e8e48cecdee6c2832be5834b11a17a6bde2c
holrock <ispeporez@gmail.com>
2015-05-26 23:03:42 +0900

    add spec

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -2,7 +2,7 @@ class TodoListsController < ApplicationController
   #before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
 
   def list
-    @todo_items = current_user.todo_items
+    @todo_items = current_user.todo_items.order("updated_at DESC")
     @new_todo = TodoItem.new
   end
 
--- spec/controllers/todo_lists_controller_spec.rb
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -3,10 +3,26 @@ require 'rails_helper'
 
 RSpec.describe TodoListsController, type: :controller do
   describe "GET #list" do
+
     context "logined" do
       before :each do
         @user = Fg.create(:user)
         login_user(@user)
+      end
+
+      it "list todo" do
+        @user.todo_items.create(Fg.attributes_for(:todo_item))
+        get :list
+        expect(assigns(:todo_items)).to match([@user.todo_items.first])
+      end
+
+      it "order by updated_at" do
+        @user.todo_items.create(Fg.attributes_for(:todo_item, text:'1'))
+        @user.todo_items.create(Fg.attributes_for(:todo_item, text:'2'))
+        @user.todo_items.create(Fg.attributes_for(:todo_item, text:'3'))
+        get :list
+        desc_sorted = @user.todo_items.sort {|a, b| b.updated_at <=> a.updated_at}
+        expect(assigns(:todo_items)).to match(desc_sorted)
       end
 
       it "render list" do
