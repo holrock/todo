5a4665a88b89cb3cbf2fbc7205ecc48db2397840
holrock <ispeporez@gmail.com>
2015-05-26 13:52:46 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524043150) do
+ActiveRecord::Schema.define(version: 20150526044437) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string   "text"
+    t.boolean  "finished"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "list_order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",            null: false
